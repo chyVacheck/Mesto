a43881ce0bbc0747cace732d6332a97f9510316a
Dmytro Shakh <work.dmytro02@gmail.com>
2022-08-28 20:07:40 +0200

    Create UserInfo.js

---
+++ functional/UserInfo.js
@@ -0,0 +1,19 @@
+
+export class UserInfo {
+  constructor(user) {
+    this._name = document.querySelector(user.name);
+    this._info = document.querySelector(user.info);
+  }
+
+  getUserInfo() {
+    return {
+      name: this._name.textContent,
+      info: this._info.textContent
+    }
+  }
+
+  setUserInfo(user) {
+    this._name.textContent = user.name;
+    this._info.textContent = user.info;
+  }
+}
