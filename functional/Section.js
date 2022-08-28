f5fdc47608128bab537305ddff4fc85b92ba2c13
Dmytro Shakh <work.dmytro02@gmail.com>
2022-08-28 20:07:44 +0200

    Create Section.js

---
+++ functional/Section.js
@@ -0,0 +1,22 @@
+
+export class Section {
+  constructor({ items, renderer }, containerSelector) {
+    this._initialArray = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+    this.addItem = this.addItem.bind(this);
+  }
+
+  //принимает DOM-элемент и добавляет его в контейнер
+  addItem(element) {
+    this._container.prepend(element);
+  }
+
+  //вызывает renderer для каждого элемента
+  renderItems() {
+    this._initialArray.forEach(item => {
+      this._renderer(item); // вызываем renderer, передав item
+    });
+  }
+
+}
