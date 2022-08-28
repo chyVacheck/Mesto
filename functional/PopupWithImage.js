a5f463f22791fc914c2d3e66fc0fcbf422ba1b5c
Dmytro Shakh <work.dmytro02@gmail.com>
2022-08-28 20:09:32 +0200

    refactor: убрать повторение кода

--- functional/PopupWithImage.js
+++ functional/PopupWithImage.js
@@ -1,17 +1,16 @@
 import { Popup } from "./Popup.js";
 
 export class PopupWithImage extends Popup {
-  constructor(popupSelector){
+  constructor(popupSelector) {
     super(popupSelector);
     this._title = this._popup.querySelector('.popup__card-title');
     this._image = this._popup.querySelector('.popup__card-image');
   }
   // открытие поп-апа
   open(src, text) {
+    super.open();
     this._title.textContent = text;
+    this._image.alt = text;
     this._image.src = src;
-    this._popup.classList.add('popup_opened');
-    this._popup.addEventListener('mousedown', this.setEventListeners.bind(this));
-    window.addEventListener('keydown', this._handleEscClose);
   }
 };
