b9d51d84f68f185f1826f80c9df7bcfa029254d1
Dmytro Shakh <work.dmytro02@gmail.com>
2022-08-28 20:14:10 +0200

    refactor: много изминений
    1 убрать функцию getTemplate
    2 заменить строковые значения на константы
    3 вынести в отдельную функцию логику закрытия поп-апа по клику

--- functional/Popup.js
+++ functional/Popup.js
@@ -1,42 +1,46 @@
 import { selectorPopupButtonClose } from './constants.js';
+import { classPopupOpened } from './constants.js';
 
-export class Popup{
-  constructor(popupSelector){
+export class Popup {
+  constructor(popupSelector) {
     this._selector = popupSelector;
-    this._popup = this._getTemplate();
+    this._popup = document.querySelector(this._selector);
+
     this._handleEscClose = this._handleEscClose.bind(this);
+    this._closeByClick = this._closeByClick.bind(this);
   }
-  
+
+  // функция зарытия поп-апа, по нажатию Esc
   _handleEscClose(event) {
     if (event.key === 'Escape') {
       this.close();
     }
   }
- 
-  setEventListeners(event) {
+
+  // функция зарытия поп-апа, по клику
+  _closeByClick(event) {
     const buttonClose = this._popup.querySelector(selectorPopupButtonClose);
     if ((event.target === this._popup) || (event.target === buttonClose)) {
       this.close();
     }
   }
 
+  // вешаем слушатели
+  setEventListeners() {
+    this._popup.addEventListener('mousedown', this._closeByClick);
+  }
+
   // открытие поп-апа
   open() {
-    this._popup.classList.add('popup_opened');
-    this._popup.addEventListener('mousedown', this.setEventListeners.bind(this));
+    this._popup.classList.add(classPopupOpened);
+    this.setEventListeners();
     window.addEventListener('keydown', this._handleEscClose);
   }
 
   // закрытие поп-апа
   close() {
     window.removeEventListener('keydown', this._handleEscClose);
-    this._popup.classList.remove('popup_opened');
-  }
-
-  // поиск template
-  _getTemplate() {
-    const popup = document.querySelector(this._selector)
-    return popup;    
+    this._popup.classList.remove(classPopupOpened);
   }
 
 };
