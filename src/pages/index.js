52993a32d3d00c0abadde450c056e0c210bbc19c
Dmytro Shakh <work.dmytro02@gmail.com>
2022-08-29 21:02:54 +0200

    refactor: облегчение кода, записать новые методы вместо функций убрать не нужные переменные

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import '/src/pages/index.css'; // добавьте импорт главного
 //* import from components 
 import { Card } from '../components/Card.js';
 import { FormValidator } from '../components/FormValidator.js';
-// import { Popup } from '../components/Popup.js';
 import { PopupWithForm } from '../components/PopupWithForm.js';
 import { PopupWithImage } from '../components/PopupWithImage.js';
 import { Section } from '../components/Section.js';
@@ -14,7 +13,6 @@ import { UserInfo } from '../components/UserInfo.js';
 import { initialCards, form } from '../utils/constants.js';
 
 const page = document.querySelector('.page');
-const content = page.querySelector('.content');
 
 //* profile
 const user = new UserInfo({
@@ -22,7 +20,7 @@ const user = new UserInfo({
   info: '.profile__description',
 });
 
-const profile = content.querySelector('.profile');
+const profile = page.querySelector('.profile');
 const profileInfo = profile.querySelector('.profile__info');
 // all buttons in profile
 const profileEditButton = profileInfo.querySelector('.profile__edit-button');   //button
@@ -30,40 +28,20 @@ const profileAddNewCardButton = profile.querySelector('.profile__add-button');  
 
 //* elements
 const cardListSection = '.elements__list-cards';
-const elements = content.querySelector('.elements');
-const elementsListCard = elements.querySelector(cardListSection);
 
 //* pop-up Edit
 const popupProfileEdit = new PopupWithForm('#popup-edit', submitFormForPopupEdit);
 popupProfileEdit.setEventListeners();
 
-let formEdit = {};
-Object.assign(formEdit, form)
-formEdit.formSelector = '#edit-popup-form';
-const objEditForm = new FormValidator(formEdit);
-
-const popupEdit = page.querySelector('#popup-edit');
-const popupEditContainer = popupEdit.querySelector('#popup-edit-container');
-
-const popupEditForm = popupEditContainer.querySelector('#edit-popup-form');
-const popupEditFormName = popupEditForm.querySelector('#edit-input-name');
-const popupEditFormInfo = popupEditForm.querySelector('#edit-input-info');
+const popupEditForm = page.querySelector('#edit-popup-form');
+const objEditForm = new FormValidator(form, popupEditForm);
 
 //* pop-up Add
 const popupCardAdd = new PopupWithForm('#popup-add', submitFormForPopupAdd);
 popupCardAdd.setEventListeners();
 
-let formAdd = {};
-Object.assign(formAdd, form)
-formAdd.formSelector = '#add-popup-form';
-const objAddForm = new FormValidator(formAdd);
-
-const popupAdd = page.querySelector('#popup-add');
-const popupAddContainer = popupAdd.querySelector('#popup-add-container');
-
-const popupAddForm = popupAddContainer.querySelector('#add-popup-form');
-const popupAddFormName = popupAddForm.querySelector('#add-input-name');
-const popupAddFormInfo = popupAddForm.querySelector('#add-input-info');
+const popupAddForm = page.querySelector('#add-popup-form');
+const objAddForm = new FormValidator(form, popupAddForm);
 
 //* pop-up Card
 const popupWithImage = new PopupWithImage('#popup-card');
@@ -91,10 +69,7 @@ const cardList = new Section(
  */
 function submitFormForPopupEdit() {
   //перезаписываем значения в profileIndo взятое из popupEditForm...
-  user.setUserInfo({
-    name: popupEditFormName.value,
-    info: popupEditFormInfo.value,
-  });
+  user.setUserInfo(popupProfileEdit.getInputValues());
   popupProfileEdit.close();
 }
 
@@ -107,10 +82,7 @@ function submitFormForPopupEdit() {
  */
 function submitFormForPopupAdd(evt) {
   evt.preventDefault();
-  const card = {
-    name: popupAddFormName.value,
-    link: popupAddFormInfo.value,
-  }
+  const card = popupCardAdd.getInputValues();
   //создаем карточку для добавления
   const newCard = new Card('#template-сard', card, () => {
     popupWithImage.open(card.link, card.name);
@@ -121,23 +93,17 @@ function submitFormForPopupAdd(evt) {
 
 // слушатель на кнопку редактирования профиля
 profileEditButton.addEventListener('click', () => {
-  const userInfo = user.getUserInfo();
-  //записываем в editFormInput значение из profile
-  popupEditFormName.value = userInfo.name;
-  popupEditFormInfo.value = userInfo.info;
-  objEditForm.showError(popupEditFormName);
-  objEditForm.showError(popupEditFormInfo);
+  popupProfileEdit.setInputValues(user.getUserInfo());
+  objEditForm.resetValidation();
   objEditForm.setSubmitButtonState(true);
   popupProfileEdit.open();
 });
 
 // слушатель на кнопку создания новой карточки
 profileAddNewCardButton.addEventListener('click', () => {
-  popupCardAdd.form.reset();
-  objAddForm.setSubmitButtonState(false);
+  objAddForm.resetValidation();
   popupCardAdd.open();
 });
-
 
 //включаем валидацию на формы
 objEditForm.enableValidation();
